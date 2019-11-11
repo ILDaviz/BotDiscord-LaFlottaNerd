d878bdaffb2f6f81541fc8884ea04ea87fb79891
David <davidgalet@gmail.com>
2019-11-11 13:00:10 +0100

    upd

--- helpers/Models.js
+++ helpers/Models.js
@@ -4,33 +4,60 @@ var db_users = require('../helpers/Dbjson').users;
 var db_settings = require('../helpers/Dbjson').settings;
 var db_names = require('../helpers/Dbjson').livel_names;
 var db_texts = require('../helpers/Dbjson').texts;
-var db_users = require('../helpers/Dbjson').users;
-
 
 var Models = function () {
     //Costructor
 };
 
-Models.addUsers = function (data_user ,result) {
+
+Models.Users = function (
+    method,
+    id_discord, 
+    notified = 0, 
+    messages = 0, 
+    messages_day = 0, 
+    presences = 0, 
+    presence_day = 0, 
+    presence_update = 0, 
+    presence_message = 0, 
+    presence_reaction = 0, 
+    last_check = null, 
+    bot_mention = 0, 
+    rule_selected = 0, 
+    result) {
   //Crea la nuova chiave di inserimento
   let new_id = db_users.get('last_counter') + 1;
-  //
-  db.get('users')
-    .push(
-        {"id_users":new_id,
-        "id_discord":"537249527170727959",
-        "notified":"0",
-        "messages":"124",
-        "messages_day":"7",
-        "presences":"668",
-        "presence_day":"23",
-        "presence_update":"896",
-        "presence_message":"419",
-        "presence_reaction":"193",
-        "last_check":null,
-        "bot_mention":"0",
-        "rule_selected":"0"
-      }).write()
+  
+  switch (method) {
+        case 'push':
+            db.get('users')
+            .push(
+                {"id_users":new_id,
+                "id_discord":id_discord,
+                "notified": notified,
+                "messages": messages,
+                "messages_day": messages_day,
+                "presences": presences,
+                "presence_day": presence_day,
+                "presence_update": presence_update,
+                "presence_message": presence_message,
+                "presence_reaction": presence_reaction,
+                "last_check": last_check,
+                "bot_mention": bot_mention,
+                "rule_selected": rule_selected
+            }).write().then( value => {
+                console.log(value); // Success!
+            }, reason => {
+                console.log(reason); // Error!
+            } );
+            break;
+        case 'get':
+            
+            break;
+
+      default:
+          break;
+  }
 }
 
 
